2635cb88d21249c34ca0de5cd8c6ac4b81d41478
raydbasa <s441006995@st.uqu.edu.sa>
2023-12-10 14:07:40 +0300

    delet

--- blog/views.py
+++ blog/views.py
@@ -1,13 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from django.template import loader
 from .models import Post, Comment, Category
 from django.contrib.auth.models import User
 
-
-def main(request):
-    template = loader.get_template('main.html')
-    return HttpResponse(template.render())
 
 def index(request):
     return render(request, 'index.html')
